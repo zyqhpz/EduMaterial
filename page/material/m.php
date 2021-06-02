55a6fe2734eecaa7e83d3471e0b0fe0826bdcf70
Haziq Hapiz <zyqq.dev@gmail.com>
2021-06-02 16:39:11 +0800

    Update m.php

--- page/material/m.php
+++ page/material/m.php
@@ -4,56 +4,130 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>EduMaterial | Material</title>
+    <link rel="stylesheet" type="text/css" href="../../src/css/header.css">
+    <link rel="stylesheet" type="text/css" href="../../src/css/material.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    
+    <nav id="header">
+        <a href="/index.php" class="logo">EduMaterial</a>
+        <div class="toggle"></div>
+        <ul class="navigation">
+            <li><a href="../../index.php">Home</a></li>
+            <li><a href="../../page/donate/donate.php">Donate</a></li>
+            <li><a href="../../math.php" class="active">Material</a></li>
+            <li><a href="../../page/about-us/about_us.php">About Us</a></li>
+            <li><a href="../../page/login/login.php">Login</a></li>
+        </ul>
+    </nav>
 </head>
-<style>
-table {
-border-collapse: collapse;
-width: 100%;
-color: #588c7e;
-font-family: monospace;
-font-size: 25px;
-text-align: left;
-}
-th {
-background-color: #588c7e;
-color: white;
-}
-tr:nth-child(even) {background-color: #f2f2f2}
-</style>
 <body>
-<table>
-<tr>
-<th>Material Id</th>
-<th>Material Name</th>
-<th>Material Type</th>
-</tr>
-<?php
+    <div class="sidebar">
+        <h1 id="title">Subjects</h1>
+        <ul id="subject-list">
+            <li class="active">
+                <a href="#">
+                    <i class="fas fa-calculator"></i>
+                    <span class="link-text">Mathematics</span>
+                </a>
+            </li>
+            <li>
+                <a href="sci.php">
+                    <i class="fas fa-flask"></i>
+                    <span class="link-text">Science</span>
+                </a>
+            <li>
+                <a href="cs.php">
+                    <i class="fas fa-laptop-code"></i>
+                    <span class="link-text">Computer Science</span>
+                </a>
+            </li>
+            <li>
+                <a href="phi.php">
+                    <i class="fas fa-book"></i>
+                    <span class="link-text">Philosophy</span>
+                </a>
+            </li>
+            <li>
+                <a href="art.php">
+                    <i class="fas fa-paint-brush"></i>
+                    <span class="link-text">Art</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div class="main">
+        <h2>Mathematics</h2>
+        <div class="topic-list">
+            <div id="video">
+                <ul>
+                    <h3>Videos</h3>
+                    <li><a href="https://www.youtube.com/watch?v=HfACrKJ_Y2w" target="_blank">Calculus</a> by Mr. A</li>
+                    <li><a href="https://www.youtube.com/watch?v=LwCRRUa8yTU" target="_blank">Algebra</a> by Mrs. B</li>
+                    <li><a href="https://www.youtube.com/watch?v=xxpc-HPKN28" target="_blank">Statistics</a> by Ms. C</li>
+                </ul>
+            </div>
+            <div id="book">
+                <ul>
+                    <h3>E-Book</h3>
+                    <li><a href="https://ocw.mit.edu/ans7870/resources/Strang/Edited/Calculus/Calculus.pdf" target="_blank">Calculus</a> by Mr. A</li>
+                    <li><a href="https://www.math.stonybrook.edu/~aknapp/download/b2-alg-inside.pdf" target="_blank">Algebra</a> by Mrs. B</li>
+                    <li><a href="http://www.utstat.toronto.edu/mikevans/jeffrosenthal/book.pdf" target="_blank">Probability and Statistics</a> by Ms. C</li>
+                </ul>
+            </div>
+        </div>
+        <div class=topic-list>
+            <div id="video">
+                <ul>
+                <h3>Video</h3>
+            <?php
 
-$conn = mysqli_connect("localhost", "root", "", "webapp");
+            $conn = mysqli_connect("localhost", "root", "", "webapp");
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-    }
-$sql = "SELECT * FROM material";
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+                }
+            $sql = "SELECT * FROM material, user";
 
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-    $link = $row["material_file"];
-    $type = $row["material_type"];
-    echo "<tr><td>" . $row["material_id"]. "</td><td>" . $row["material_name"] . "</td><td>"
-    . $row["material_type"]. "</td><td>" . "<a target=_blank href=" . $link. ">$link</a>" . "</td></tr>";
-    echo "<tr><td>" . $row["material_id"]. "</td><td>" . $row["material_name"] . "</td><td>"
-    . "<a target=_blank href=" . $link . ">$type</a>" . "</td></tr>";
-    }
-    echo "</table>";
-    } else { echo "0 results"; }
-    $conn->close();
-?>
+            $result = $conn->query($sql);
 
+            if ($result->num_rows > 0) {
+                // output data of each row
+                // echo "<div class=" . "topic-list" . ">" .
+                //             "<div id=" .$type. ">" .
+                //                 "<ul>" .
+                //                     "<h3>" . ucfirst($type) ."</h3>" .
+                //                     "<li>"; orii
 
-</table>
+                // echo "<div class=" . "topic-list" . ">" .
+                //             "<div id=" . "video" . ">" .
+                //                 "<ul>" .
+                //                     "<h3>" . "Video" ."</h3>";
+                                    // "<li>";
+                while($row = $result->fetch_assoc()) {
+                $link = $row["material_file"];
+                $type = $row["material_type"];
+                $title = $row["material_name"];
+                $category = $row["category_id"];
+                $donator = ucfirst($row["user_firstname"]) . " " . ucfirst($row["user_lastname"]);
+                
+                    if ($type == "video" and $category == 1 and $row["user_firstname"] == "Jake") {
+                            
+                    echo " <li>
+                    <a target=_blank href=" . $link. ">$title</a>" . " by ". $donator ." <br> &emsp; <strong> Description </strong> : " . $row["material_description"] .
+                    "</li>";
+                }
+                    
+                }
+                echo "
+                                        </ul>
+                                    </div>
+                                </div>";
+            } else { echo "0 results"; }
+            $conn->close();
+            ?>
+    </div>
+    <script src="/src/js/header.js"></script>
 </body>
 </html>
